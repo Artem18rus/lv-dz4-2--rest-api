0afb79e560733bb2be077e6aed4ba6ff992ba497
Artem18rus <urart88@mail.ru>
2023-07-23 19:30:02 +0400

    4

---
+++ app-rest/app/Http/Controllers/ApiTokenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiTokenController extends Controller
+{
+    public function createToken(Request $request) {
+        //доделать если надо по ДЗ!!!
+    }
+}
